74c16b1881f4ebb492c4ac9a1d4c27408295d46a
maameadwoa13 <74627224+maameadwoa13@users.noreply.github.com>
2020-12-02 08:47:35 -0500

    modified userModel

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,9 +2,21 @@ const mongoose = require("mongoose");
 //const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, minlenght: 5, maxlenght: 16, unique: true },
-  email: { type: String, unique: true },
-  password: { type: String, minlenght: 8 }
+  username: { type: String, 
+    minlenght: [5, 'minimum username lenght is 5'],
+      maxlenght: [16,'maximum username lenght is 16'],
+         unique: true,
+        required:[true, 'please enter a username'],
+      lowercase:true},
+
+  email: { type: String,
+     unique: true ,
+    required:[true, 'the email field is required'],
+  lowercase:true},
+
+  password: { type: String, 
+    minlenght: 8,
+  required:[true, 'you must enter a password'] }
 });
 
 /*User.pre('save', function(next){
